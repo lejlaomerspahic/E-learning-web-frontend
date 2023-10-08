ec47fe188cb38e39aa813edb537b2fe634e212b9
Lejla Omerspahic <96488468+lejlaomerspahic@users.noreply.github.com>
2023-10-08 20:11:32 +0200

    registration

--- elearning/src/components/Registracija.js
+++ elearning/src/components/Registracija.js
@@ -2,127 +2,271 @@ import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Prijava.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-import { faFacebook } from "@fortawesome/free-brands-svg-icons";
-
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { Link, useNavigate } from "react-router-dom";
 function Registracija() {
   const navigate = useNavigate();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const handleLogin = () => {
+  const [agreeTerms, setAgreeTerms] = useState(false);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [location, setLocation] = useState("");
+  const [passwordRepeat, setPasswordRepeat] = useState("");
+
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [passwordRepeatError, setPasswordRepeatError] = useState("");
+  const [locationError, setLocationError] = useState("");
+  const [agreeTermsError, setAgreeTermsError] = useState("");
+
+  const handleRegistration = () => {
+    let hasError = false;
+
+    setNameError("");
+    setEmailError("");
+    setPasswordError("");
+    setLocationError("");
+    setPasswordRepeatError("");
+
+    if (!name) {
+      setNameError("Name is a required field.");
+      hasError = true;
+    }
+
+    if (!email) {
+      setEmailError("Email is a required field.");
+      hasError = true;
+    } else if (
+      !email.includes("@") ||
+      (!email.endsWith(".com") && !email.endsWith(".ba"))
+    ) {
+      setEmailError("Invalid email format.");
+      hasError = true;
+    }
+
+    if (!password) {
+      setPasswordError("Password is a required field.");
+      hasError = true;
+    } else if (password.length < 6) {
+      setPasswordError("Password must be at least 6 characters long.");
+      hasError = true;
+    }
+
+    if (!passwordRepeat) {
+      setPasswordRepeatError("Repeat password is a required field.");
+      hasError = true;
+    } else if (passwordRepeat !== password) {
+      setPasswordRepeatError(
+        "The passwords do not match. Please re-enter the password."
+      );
+      hasError = true;
+    }
+
+    if (!location) {
+      setLocationError("Location is a required field.");
+      hasError = true;
+    }
+
+    if (!agreeTerms) {
+      setAgreeTermsError("You must agree to the terms and conditions.");
+      hasError = true;
+    }
+
+    if (hasError) {
+      return;
+    }
     navigate("/home");
   };
+
   return (
-    <section class="vh-100">
-      <div class="container-fluid h-custom">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-          <div class="col-md-9 col-lg-6 col-xl-5">
+    <section className="vh-100">
+      <div className="container-fluid h-custom">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-md-9 col-lg-6 col-xl-5">
             <img
               src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-              class="img-fluid"
+              className="img-fluid"
               alt="Sample image"
             />
           </div>
-          <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+          <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
             <form>
-              <div class="divider d-flex align-items-center my-4">
+              <div className="divider d-flex align-items-center my-4">
                 <p
-                  class="text-center fw-bold mx-3 mb-0"
+                  className="text-center fw-bold mx-3 mb-0"
                   style={{ fontSize: "26px" }}
                 >
                   Sign up
                 </p>
               </div>
-              <div class="form-outline mb-3">
+              <div className="form-outline mb-3">
                 <input
                   type="text"
                   id="form3Example4"
-                  class="form-control form-control-lg"
+                  className="form-control form-control-lg"
                   placeholder="Name"
+                  onChange={(name) => setName(name.target.value)}
                 />
               </div>
-              <div class="form-outline mb-4">
+              {nameError ? (
+                <p
+                  style={{
+                    color: "#0d9eff",
+                    marginBottom: 3,
+                    marginTop: -15,
+                    marginLeft: 3,
+                  }}
+                >
+                  {nameError}
+                </p>
+              ) : null}
+
+              <div className="form-outline mb-4">
                 <input
                   type="email"
                   id="form3Example3"
-                  class="form-control form-control-lg"
+                  className="form-control form-control-lg"
                   placeholder="Email"
+                  onChange={(email) => setEmail(email.target.value)}
                 />
               </div>
-
-              <div class="form-outline mb-3">
+              {emailError ? (
+                <p
+                  style={{
+                    color: "#0d9eff",
+                    marginBottom: 3,
+                    marginTop: -20,
+                    marginLeft: 3,
+                  }}
+                >
+                  {emailError}
+                </p>
+              ) : null}
+              <div className="form-outline mb-3">
                 <input
                   type="password"
                   id="form3Example4"
-                  class="form-control form-control-lg"
+                  className="form-control form-control-lg"
                   placeholder="Password"
+                  onChange={(password) => setPassword(password.target.value)}
                 />
               </div>
-
-              <div class="form-outline mb-3">
+              {passwordError ? (
+                <p
+                  style={{
+                    color: "#0d9eff",
+                    marginBottom: 3,
+                    marginTop: -15,
+                    marginLeft: 3,
+                  }}
+                >
+                  {passwordError}
+                </p>
+              ) : null}
+              <div className="form-outline mb-3">
                 <input
                   type="password"
                   id="form3Example4"
-                  class="form-control form-control-lg"
+                  className="form-control form-control-lg"
                   placeholder="Repeat password"
+                  onChange={(passwordRepeat) =>
+                    setPasswordRepeat(passwordRepeat.target.value)
+                  }
                 />
               </div>
-              <div class="form-outline mb-3">
+              {passwordRepeatError ? (
+                <p
+                  style={{
+                    color: "#0d9eff",
+                    marginBottom: 3,
+                    marginTop: -15,
+                    marginLeft: 3,
+                  }}
+                >
+                  {passwordRepeatError}
+                </p>
+              ) : null}
+              <div className="form-outline mb-3">
                 <input
                   type="text"
                   id="form3Example4"
-                  class="form-control form-control-lg"
+                  className="form-control form-control-lg"
                   placeholder="Location"
+                  onChange={(location) => setLocation(location.target.value)}
                 />
               </div>
-
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="form-check mb-0">
+              {locationError ? (
+                <p
+                  style={{
+                    color: "#0d9eff",
+                    marginBottom: 3,
+                    marginTop: -15,
+                    marginLeft: 3,
+                  }}
+                >
+                  {locationError}
+                </p>
+              ) : null}
+              <div
+                className="d-flex justify-content-between align-items-center"
+                style={{ marginTop: "20px" }}
+              >
+                <div className="form-check mb-0">
                   <input
-                    class="form-check-input me-2"
+                    className="form-check-input me-2"
                     type="checkbox"
                     value=""
                     id="form2Example3"
+                    checked={agreeTerms}
+                    onChange={() => setAgreeTerms(!agreeTerms)}
                   />
-                  <p class="small  mt-1">
-                    I agree to the
+                  <p className="small  mt-1">
+                    I agree to the{" "}
                     <Link
                       to="/prijava"
                       href="#!"
                       style={{ textDecoration: "none" }}
-                      class="link-danger"
+                      className="link-danger"
                     >
-                      {" "}
                       terms & conditions
                     </Link>
                   </p>
                 </div>
               </div>
-              <div class="text-center text-lg-start mt-4 pt-2">
+              {agreeTermsError ? (
+                <p
+                  style={{
+                    color: "#0d9eff",
+                    marginBottom: 3,
+                    marginTop: -15,
+                    marginLeft: 3,
+                  }}
+                >
+                  {agreeTermsError}
+                </p>
+              ) : null}
+              <div className="text-center text-lg-start mt-4 pt-2">
                 <button
                   type="button"
-                  class="btn btn-primary btn-lg"
+                  className="btn btn-primary btn-lg"
                   style={{
                     paddingLeft: "2.5rem",
                     paddingRight: "2.5rem",
                     background: "#035dbd",
                     fontWeight: "Bold",
                   }}
-                  onClick={handleLogin}
+                  onClick={handleRegistration}
                 >
                   SIGN UP
                 </button>
-                <p class="small fw-bold mt-2 pt-1 mb-0">
+                <p className="small fw-bold mt-2 pt-1 mb-0">
                   You already have an account?{" "}
                   <Link
                     to="/prijava"
                     href="#!"
                     style={{ textDecoration: "none" }}
-                    class="link-danger"
+                    className="link-danger"
                   >
                     Sign in
                   </Link>
