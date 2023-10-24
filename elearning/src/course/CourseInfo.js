d79c00fce7f31cfb24af891150ad58310329ba35
Lejla Omerspahic <96488468+lejlaomerspahic@users.noreply.github.com>
2023-10-24 16:49:08 +0200

    implementation video course modal

--- elearning/src/course/CourseInfo.js
+++ elearning/src/course/CourseInfo.js
@@ -6,6 +6,10 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "./CourseInfo.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe, faCalendar } from "@fortawesome/free-solid-svg-icons";
+import VideoModal from "./VideoModal";
+
 const CourseInfo = () => {
   const { id } = useParams();
   const [courseInfo, setCourseInfo] = useState();
@@ -13,41 +17,84 @@ const CourseInfo = () => {
     url: `${variable}/course/${id}`,
   });
 
+  const formatLastUpdated = (date) => {
+    const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+    return new Date(date).toLocaleDateString(undefined, options);
+  };
+
   useEffect(() => {
     if (course) {
       setCourseInfo(course);
     }
   }, [course]);
 
-  console.log(course);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <div>
       <Navbar></Navbar>
       {course && (
-        <div>
-          <div className="divCaption">
+        <div className="divMain">
+          <div>
+            <h6>
+              Course &gt; {course.category} &gt; {course.name}
+            </h6>
+
             <h2> {course.name}</h2>
             <h4> {course.info}</h4>
 
             {course.instructor?.map((instructor, index) => (
-              <h5 key={index}>{instructor.name}</h5>
+              <h6 key={index}>Created by: {instructor.name}</h6>
             ))}
 
-            <div>
-              <h5></h5>
-              <h5></h5>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                flexDirection: "row",
+                width: "500px",
+              }}
+            >
+              <h6>
+                <FontAwesomeIcon icon={faCalendar} /> Last updated:{" "}
+                {formatLastUpdated(course.lastUpdated)}
+              </h6>
+              <h6>
+                <FontAwesomeIcon icon={faGlobe} /> Language: {course.language}
+              </h6>
             </div>
           </div>
 
-          <iframe
-            width="560"
-            height="315"
-            src={`https://www.youtube.com/embed/${course.videoId}`}
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          ></iframe>
+          <div
+            style={{
+              backgroundImage: `url(${course.imageUrl})`,
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+            }}
+            className="imageDiv"
+          >
+            <img
+              style={{
+                height: "25%",
+              }}
+              onClick={openModal}
+              src="https://www.freeiconspng.com/uploads/youtube-logo-png-hd-14.png"
+            ></img>
+          </div>
+          <VideoModal
+            isOpen={modalIsOpen}
+            course={course}
+            closeModal={closeModal}
+          />
         </div>
       )}
       <Footer></Footer>
---
+++ elearning/src/course/VideoModal.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Modal from "react-modal";
+import "./VideoModal.css";
+
+const VideoModal = ({ isOpen, closeModal, course }) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={closeModal}
+      overlayClassName="overlay"
+      className="videoModal"
+    >
+      <iframe
+        width="1200"
+        height="600"
+        style={{ borderRadius: "20px" }}
+        src={`https://www.youtube.com/embed/${course.videoId}`}
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </Modal>
+  );
+};
+
+export default VideoModal;
