83ba3693aadf871ae3a1182d76ff48767ec84ed7
Lejla Omerspahic <96488468+lejlaomerspahic@users.noreply.github.com>
2023-11-07 00:36:35 +0100

    cart

--- elearning/src/cart/Cart.js
+++ elearning/src/cart/Cart.js
@@ -2,29 +2,84 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import "./Cart.css";
 import Footer from "../components/Footer";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+import { faTruck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useUser } from "../hook/useUser";
 function Cart() {
-  const [data, setData] = useState("");
+  const { user } = useUser();
+  const [data, setData] = useState(null);
   const storedData = localStorage.getItem("cartData");
+
   useEffect(() => {
     if (storedData) {
-      setData(storedData);
+      const parsedData = JSON.parse(storedData);
+      setData(parsedData);
     }
   }, [storedData]);
 
-  console.log("data");
-  console.log(data);
   return (
     <div className="cart-container">
       <Navbar></Navbar>
-      {data.cartItems?.map((cartItem, index) => (
-        <div key={index} className="product-container">
-          <img src={cartItem.product.imageUrl} alt={cartItem.product.title} />
-          <h3>{cartItem.product.title}</h3>
-          <p>Supplier: {cartItem.product.supplier}</p>
-          <p>Price: {cartItem.product.price}</p>
-          <p>Description: {cartItem.product.description}</p>
-        </div>
-      ))}
+
+      <h3 style={{ margin: "20px", fontWeight: "600" }}>Shopping cart</h3>
+      <div className="product-list">
+        {data &&
+          data.cartItems?.map((cartItem, index) => (
+            <div key={index} className="product-containerrr">
+              <div style={{ width: "160px", height: "190px" }}>
+                <img
+                  className="product-imageee"
+                  src={cartItem.product.imageUrl}
+                  alt={cartItem.product.title}
+                />
+              </div>
+              <div className="product-info">
+                <h3>{cartItem.product.title}</h3>
+                <p>Supplier: {cartItem.product.supplier}</p>
+                <div className="location">
+                  <FontAwesomeIcon icon={faMapMarkerAlt} />
+                  <p>{cartItem.product.productLocation}</p>
+                </div>
+                <div
+                  style={{
+                    height: "0.5px",
+                    backgroundColor: "gray",
+                    marginTop: "10px",
+                  }}
+                ></div>
+                <div
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <div
+                    style={{ display: "flex", justifyContent: "flex-start" }}
+                  >
+                    Price:
+                    <p
+                      style={{
+                        color: "green",
+                        marginLeft: "5px",
+                        fontWeight: "700",
+                      }}
+                    >
+                      {" "}
+                      {cartItem.product.price}
+                    </p>
+                  </div>
+                  <div>
+                    <FontAwesomeIcon
+                      icon={faTruck}
+                      style={{ marginRight: "5px" }}
+                    />
+                    {user.user.location === cartItem.product.productLocation
+                      ? "Free Delivery"
+                      : "10$"}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
       <Footer></Footer>
     </div>
   );
